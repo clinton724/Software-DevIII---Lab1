7e07c5ce2f33f762cad8805f06bfc89760c63b5b
Clinton <clintonletsoela724c@gmail.com>
2021-08-03 10:58:30 +0200

    typescript setup fixed

--- app/src/app.ts
+++ app/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 const app = express()
 
 const http = require('http')
+const db = require('./db.js')
 const server = http.createServer(app)
 
 module.exports = { server, app }
