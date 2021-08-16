2599385d3f019594263276cc94c54db6f796177b
Clinton <clintonletsoela724c@gmail.com>
2021-08-17 01:55:20 +0200

    password hashing

--- app/src/controllers/userManager.js
+++ app/src/controllers/userManager.js
@@ -1,4 +1,6 @@
 const { validationResult } = require('express-validator')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
 
 class userManager {
   constructor ({ userRepository }) {
@@ -18,6 +20,20 @@ class userManager {
     if (temp.length === 1) {
       return res.status(400).json({ errors: 'the email already exists' })
     }
+    const repo = this.userRepository
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) {
+        console.log(err)
+      }
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        // Store hash in your password DB.
+        user.password = hash
+        if (err) {
+          console.log(err)
+        }
+        repo.addUser(user)
+      })
+    })
   }
 }
 
